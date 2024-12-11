8a206a4cd040e957236a1a5d84f983d4749a7304
Ahmad Faiz Agustianto <107536877+Mifaki@users.noreply.github.com>
2024-12-11 22:59:51 +0700

    feat(setup): initialize shadcn

---
+++ src/lib/fonts.ts
@@ -0,0 +1,9 @@
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  variable: '--font-poppins',
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
+export const PoppinsVariable = `${poppins.variable}`;
